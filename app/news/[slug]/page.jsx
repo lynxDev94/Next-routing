c03719a23cd1a3597b188e087066253af8fe7420
lynxDev94 <lynxpounceweb@gmail.com>
2025-05-21 21:13:44 +0300

    add some links

---
+++ app/news/[slug]/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Page = ({params}) => {
+    
+  return (
+    <div>{params.slug}</div>
+  )
+}
+
+export default Page
---
+++ app/news/page.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <h1>News Page</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/news/first">First Link</Link>
+            <Link href="/news/second">Second Link</Link>
+            <Link href="/news/third">Third Link</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Page;
